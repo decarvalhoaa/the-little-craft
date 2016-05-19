00725ae22370c17c22f753be43f312d569d49f59
Antonio de Carvalho <decarvalhoaa@gmail.com>
2016-05-19 20:58:50 +0200

    Change prio tlc_cleanup_advance_free_shipping_label

--- functions.php
+++ functions.php
@@ -848,12 +848,13 @@ add_filter( 'woocommerce_min_password_strength', 'tlc_reduce_woocommerce_min_str
 function tlc_cleanup_advance_free_shipping_label( $label, $method ) {
 
     if ( $method && isset( $method->id ) && $method->id == 'advanced_free_shipping' ) {
+
 	$label = isset( $method->label ) ? __( $method->label, 'woocommerce' ) : $label;
     }
 
     return $label;
 }
-add_filter( 'woocommerce_cart_shipping_method_full_label', 'tlc_cleanup_advance_free_shipping_label', 10, 2 );
+add_filter( 'woocommerce_cart_shipping_method_full_label', 'tlc_cleanup_advance_free_shipping_label', 1, 2 );
 
 /**
  * Events Maker
