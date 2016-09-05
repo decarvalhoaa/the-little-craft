55316f58d7a87b82cd35fce611147f9df57a7f5c
Antonio de Carvalho <decarvalhoaa@gmail.com>
2016-09-05 19:07:35 +0200

    Translate WooCommerce Breadcrumbs home url

--- functions.php
+++ functions.php
@@ -776,3 +776,12 @@ function tlc_social_meta() {
 }
 add_action('wp_head','tlc_social_meta');
 
+
+/**
+ * Translate WooCommerce Breadcrumbs home url
+ * @todo	Port to WooPoly
+ */
+function woo_custom_breadrumb_home_url() {
+	return pll_home_url();
+}
+add_filter( 'woocommerce_breadcrumb_home_url', 'woo_custom_breadrumb_home_url' );
